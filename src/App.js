f9a731023661efd878708ea4588982bd9bc3726d
laps78 <sovtem@gmail.com>
2023-03-08 20:22:36 +0300

    CRUD form created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import CRUD_form from './components/CRUD_form/CRUD_form.js'
 
 function App() {
+  const [ actualCRUDS, setAcualCruds ] = useState([]);
+  const addCrud = (newCrud) => setAcualCruds(prevCruds => [...prevCruds, newCrud]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CRUD_form addCrud={addCrud} />
     </div>
   );
 }
---
+++ src/components/CRUD_form/CRUD_form.js
@@ -0,0 +1,18 @@
+import './CRUD_form.css' ;
+import { nanoid } from 'nanoid';
+
+function CRUD_form ({addCrud}) {
+  const formSubmitHandler = (evt) => {
+    evt.preventDefault();
+    const formCurrentValue = evt.target.textarea.value;
+  }
+ return (
+   <div className="CRUD_form_wrapper">
+    <form className="CRUD_form" onSubmit={formSubmitHandler}>
+      <label htmlFor="textarea"></label>
+      <input type="textarea" name="textarea" placeholder={``}></input>
+    </form>
+   </div>); 
+}
+
+export default CRUD_form;
