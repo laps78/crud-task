1ac5488bb9f0b13d222b37133e1368b4825d44ad
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-04-16 10:35:02 +0300

    основное пофиксил. Остается проблема рассинхронизации обновления state при запросах delete

--- src/App.js
+++ src/App.js
@@ -1,92 +1,3 @@
-// import "./App.css";
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import CrudForm from "./components/CrudForm/CrudForm";
-// import CrudDesk from "./components/CrudDesk/CrudDesk";
-
-// const serverURL = "http://localhost:7777/notes";
-// const DEFAULT_MESSAGE = {
-//   message: "test",
-// };
-// let state = { notes: [], noteObj: DEFAULT_MESSAGE };
-
-// function App() {
-//   const [actualCRUDS, setActualCRUDS] = useState([]);
-//   const getData = async function () {
-//     try {
-//       let response = await fetch(serverURL, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       });
-//       let data = await response.json();
-//       console.log("app.getData: data - ", data);
-//       setActualCRUDS(data);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   const postData = () => {
-//     const responce = fetch(serverURL, {
-//       method: "POST",
-//       body: JSON.stringify(state.noteObj),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-
-//       // const responce = await fetch(serverURL, {
-//       //   headers: {
-//       //     "Content-Type": "application/json",
-//       //   },
-//       //   method: "POST",
-//       //   body: JSON.stringify(data),
-//     });
-//     //return responce.json();
-//   };
-
-//   const addCRUD = (newCrud) =>
-//     setActualCRUDS((prevCruds) => {
-//       console.log(`adding... `, newCrud);
-//       postData(serverURL, newCrud);
-//       return [...prevCruds, newCrud];
-//     });
-
-//   const deleteCRUD = (id) => {
-//     // delete from state
-//     console.log(`deleting crud ${id}`);
-//     // update request
-//     console.log(`update request (${id}) ...`);
-//     return true;
-//   };
-
-//   return (
-//     <div className="App">
-//       <p>
-//         Beshure do not forget to start server at
-//         <strong>localhost:7777</strong>
-//         <br />
-//         by typing <strong>"npm start"</strong>
-//         <br />
-//         in your terminal being at
-//         <strong>
-//           "~/Проекты/GITHUB/Netology-hw/ra16-homeworks/lifecycle-http/crud-task/backend"
-//         </strong>
-//       </p>
-//       <hr />
-//       <CrudDesk cruds={actualCRUDS} deleteCRUD={deleteCRUD} />
-//       <CrudForm addCRUD={addCRUD} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
 import React from "react";
 import { useEffect, useState } from "react";
@@ -104,7 +15,7 @@ function App() {
         },
       });
       let data = await response.json();
-      setActualCRUDS((prevCruds) => data);
+      setActualCRUDS((prevCruds) => [...data]);
     } catch (err) {
       console.log(err);
     }
@@ -115,38 +26,42 @@ function App() {
   }, []);
 
   const postData = async (url = "http://localhost:7777/notes", data = {}) => {
-    await fetch(url, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-  };
-
-  const addCRUD = (newCrud) =>
-    setActualCRUDS((prevCruds) => {
-      postData("http://localhost:7777/notes", newCrud);
-      return [...prevCruds, newCrud];
-    });
-
-  const deleteCRUD = (id) => {
-    // delete from state
-    setActualCRUDS((prevCruds) => {
-      asyncDelete(`http://localhost:7777/notes/${id}`, { id });
-      return [...prevCruds.filter((crud) => crud.id !== id)];
-    });
-    // delete request
-    const asyncDelete = async (url, data = {}) => {
+    try {
       await fetch(url, {
-        method: "DELETE",
         headers: {
           "Content-Type": "application/json",
         },
+        method: "POST",
+        body: JSON.stringify(data),
       });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addCRUD = (newCrud) => {
+    postData("http://localhost:7777/notes", newCrud)
+      .then(getData())
+      .catch((err) => console.log(err));
+  };
+
+  const deleteCRUD = (id) => {
+    // delete request
+    const asyncDelete = async (url, data = {}) => {
+      try {
+        await fetch(url, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then(getData());
+      } catch (err) {
+        console.log(err);
+      }
     };
-    // update request
-    getData();
+
+    // delete from server & update
+    asyncDelete(`http://localhost:7777/notes/${id}`, { id });
   };
 
   return (
